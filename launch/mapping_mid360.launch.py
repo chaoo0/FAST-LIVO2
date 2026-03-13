860255f00c57a5763e2b543726407902d91051f1
chaoo0 <3112315759@qq.com>
2026-03-13 15:04:38 +0800

    # 配置mid360

--- launch/mapping_mid360.launch.py
+++ launch/mapping_mid360.launch.py
@@ -15,11 +15,11 @@ def generate_launch_description():
     config_file_dir = os.path.join(get_package_share_directory("fast_livo"), "config")
     rviz_config_file = os.path.join(get_package_share_directory("fast_livo"), "rviz_cfg", "fast_livo2.rviz")
 
-    #Load parameters
-    avia_config_cmd = os.path.join(config_file_dir, "avia.yaml")
-    camera_config_cmd = os.path.join(config_file_dir, "camera_pinhole_o.yaml")
+    #这里我们修改加载的雷达参数配置文件：mid360.yaml
+    avia_config_cmd = os.path.join(config_file_dir, "mid360.yaml")
+    camera_config_cmd = os.path.join(config_file_dir, "camera_pinhole.yaml")
 
-    # Param use_rviz
+    # 打开 use_rviz
     use_rviz_arg = DeclareLaunchArgument(
         "use_rviz",
         default_value="True",
@@ -38,7 +38,6 @@ def generate_launch_description():
         description='Full path to the ROS2 parameters file to use for vikit_ros nodes',
     )
 
-    # https://github.com/ros-navigation/navigation2/blob/1c68c212db01f9f75fcb8263a0fbb5dfa711bdea/nav2_bringup/launch/navigation_launch.py#L40
     use_respawn_arg = DeclareLaunchArgument(
         'use_respawn', 
         default_value='True',
@@ -54,26 +53,19 @@ def generate_launch_description():
         camera_config_arg,
         use_respawn_arg,
 
-        #play ros2 bag
-        ExecuteProcess(
-            cmd=[['ros2 bag play ', '~/文档/rosbag/Retail_Street ', '--clock ', "-l"]], 
-            shell=True
-        ),
-
-        # republish compressed image to raw image
-        # https://robotics.stackexchange.com/questions/110939/how-do-i-remap-compressed-video-to-raw-video-in-ros2
-        # ros2 run image_transport republish compressed raw --ros-args --remap in:=/left_camera/image --remap out:=/left_camera/image
         Node(
             package="image_transport",
             executable="republish",
             name="republish",
-            arguments=[ # Array of strings/parametric arguments that will end up in process's argv
+            arguments=[ 
                 'compressed', 
                 'raw',
             ],
             remappings=[
-                ("in",  "/left_camera/image"), 
-                ("out", "/left_camera/image")
+                #("in",  "/image_raw/compressed"), 
+                #("out", "/image_raw/compressed")
+                ("in",  "/image_raw"), 
+                ("out", "/image_raw")
             ],
             output="screen",
             respawn=use_respawn,
@@ -87,11 +79,6 @@ def generate_launch_description():
                 avia_params_file,
                 camera_params_file,
             ],
-            # https://docs.ros.org/en/humble/How-To-Guides/Getting-Backtraces-in-ROS-2.html
-            prefix=[
-                # ("gdb -ex run --args"),
-                # ("valgrind --log-file=./valgrind_report.log --tool=memcheck --leak-check=full --show-leak-kinds=all -s --track-origins=yes --show-reachable=yes --undef-value-errors=yes --track-fds=yes")
-            ],
             output="screen"
         ),
 
@@ -104,3 +91,5 @@ def generate_launch_description():
             output="screen"
         ),
     ])
+
+
