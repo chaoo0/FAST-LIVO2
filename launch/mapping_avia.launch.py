94cd3f59aa6c071b328a32b012fb2413b5f4485e
chaoo0 <3112315759@qq.com>
2026-05-09 23:51:48 +0800

    Add MambaPose ONNX backend integration and safety tests

--- launch/mapping_avia.launch.py
+++ launch/mapping_avia.launch.py
@@ -65,10 +65,10 @@ def generate_launch_description():
         use_respawn_arg,
 
         # play ros2 bag
-        # ExecuteProcess(
-        #     cmd=[['ros2 bag play ', '~/datasets/Retail_Street ', '--clock ', "-l"]], 
-        #     shell=True
-        # ),
+        ExecuteProcess(
+            cmd=[['ros2 bag play ', '~/文档/rosbag/Retail_Street ', '--clock ', "-l"]], 
+            shell=True
+        ),
 
         # republish compressed image to raw image
         # https://robotics.stackexchange.com/questions/110939/how-do-i-remap-compressed-video-to-raw-video-in-ros2
